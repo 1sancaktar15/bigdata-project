793499a16dfd9631b07301bd2c8377a273776bb1
elif <ozunelif0@gmail.com>
2025-07-04 14:52:31 +0300

    add Kafka ve MinIO için eksik paketleri ve endpoint seçimini

--- spark-app/pyspark_minio_writer.py
+++ spark-app/pyspark_minio_writer.py
@@ -3,19 +3,28 @@ from pyspark.sql.functions import from_json, col
 from pyspark.sql.types import StructType, StructField, StringType, DoubleType, IntegerType, ArrayType, TimestampType
 import os
 
-# MinIO konfigürasyonu
+# MinIO konfigürasyonu (bazı Spark sürümlerinde gerekebilir)
 os.environ['AWS_ACCESS_KEY_ID'] = 'minioadmin'
 os.environ['AWS_SECRET_ACCESS_KEY'] = 'minioadmin'
 
+# Eğer Spark Docker'da ise endpoint "http://minio:9000" olmalı
+# Eğer Spark kendi bilgisayarında ise endpoint "http://localhost:9000" olmalı
+minio_endpoint = "http://localhost:9000"
+
 spark = SparkSession.builder \
     .appName("KafkaToMinIO") \
+    .config("spark.jars.packages",
+            "org.apache.spark:spark-sql-kafka-0-10_2.12:3.4.1,"
+            "org.apache.hadoop:hadoop-aws:3.3.4,"
+            "com.amazonaws:aws-java-sdk-bundle:1.12.262") \
     .config("spark.hadoop.fs.s3a.access.key", "minioadmin") \
     .config("spark.hadoop.fs.s3a.secret.key", "minioadmin") \
-    .config("spark.hadoop.fs.s3a.endpoint", "http://minio:9000") \
+    .config("spark.hadoop.fs.s3a.endpoint", minio_endpoint) \
     .config("spark.hadoop.fs.s3a.path.style.access", "true") \
     .config("spark.hadoop.fs.s3a.impl", "org.apache.hadoop.fs.s3a.S3AFileSystem") \
     .getOrCreate()
 
+# PurchasedItem JSON şeması
 schema = StructType([
     StructField("SessionId", StringType()),
     StructField("TimeStamp", TimestampType()),
@@ -33,6 +42,7 @@ schema = StructType([
     ))
 ])
 
+# Kafka'dan okuma
 df = spark.readStream \
     .format("kafka") \
     .option("kafka.bootstrap.servers", "kafka:9092") \
@@ -40,10 +50,12 @@ df = spark.readStream \
     .option("startingOffsets", "earliest") \
     .load()
 
+# JSON parse
 parsed_df = df.select(
     from_json(col("value").cast("string"), schema).alias("data")
 ).select("data.*")
 
+# MinIO'ya Parquet olarak yaz
 query = parsed_df.writeStream \
     .format("parquet") \
     .option("path", "s3a://purchased-items/") \
